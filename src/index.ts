5245a14bcc6643c5e24fd962dd0c57149b2f265b
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-07-02 23:41:08 +0300

    feat: create server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import http from 'http';
 
+const server = http.createServer((req, res) => {});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server has been started on ${PORT}...`);
+});
